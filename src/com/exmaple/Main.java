840e84ad22a4c1931f3dc0f82259e461f874ebe0
Moritz.Dilg <moritz@dilg-online.de>
2021-11-16 16:40:01 +0100

    16-11-2021

--- src/com/exmaple/Main.java
+++ src/com/exmaple/Main.java
@@ -1,12 +1,13 @@
 package com.exmaple;
 
 import com.exmaple.arrays.*;
+import com.exmaple.classes.Classes;
 import com.exmaple.strings.*;
 import com.exmaple.search.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Strings_1.strings();
+        Classes.classes();
     }
 }
--- src/com/exmaple/strings/Strings_1.java
+++ src/com/exmaple/strings/Strings_1.java
@@ -5,6 +5,13 @@ public class Strings_1 {
         //Call methods
     }
 
+    private static int count(String s, String toFind) {
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+
+        return 0;
+    }
 
     private static String toUpper(String s, String toUpperCase) {
         return s.replaceAll(toUpperCase, toUpperCase.toUpperCase());
