0d774c2d0c5c6a54a7016c7d37cf002960f53105
Moritz.Dilg <moritz@dilg-online.de>
2022-01-12 17:47:39 +0100

    12-01-2022

--- src/com/exmaple/Main.java
+++ src/com/exmaple/Main.java
@@ -3,6 +3,7 @@ package com.exmaple;
 import com.exmaple.arrays.*;
 import com.exmaple.carparts.AutoParts;
 import com.exmaple.classes.Classes;
+import com.exmaple.inheritance.newsfeed.NewsFeed;
 import com.exmaple.inheritance.schulApp.SchulApp;
 import com.exmaple.inheritance.ticketForEvents.TicketMain;
 import com.exmaple.strings.*;
@@ -13,6 +14,6 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        SchulApp.schulApp();
+        NewsFeed.newsFeed();
     }
 }
