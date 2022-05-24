c441e656d2ebfdfaa1b0e0e2181ad5536f36e898
Moritz-Dilg <moritz@dilg-online.de>
2022-05-24 16:18:54 +0200

    24-05-2022

---
+++ src/com/exmaple/generics/sortedList/SortedList.java
@@ -0,0 +1,43 @@
+package com.exmaple.generics.sortedList;
+
+import java.util.Iterator;
+
+public class SortedList<T extends Comparable<T>> implements Iterable<T> {
+    private T[] data;
+    private int nElements = 0;
+
+    public void add(T el) {
+        int i = nElements - 1;
+        while (i >= 0 && el.compareTo(data[i]) > 0) {
+            data[i + 1] = data[i];
+            i--;
+        }
+        data[i + 1] = el;
+        nElements++;
+    }
+
+    public void remove(T el) {
+        for (int i = 0; i < data.length && el.compareTo(data[i]) != 0; i++) {
+            for (int j = 0; j < data.length - 1; j++) {
+                data[j] = data[j + 1];
+            }
+            return;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            final int curr = 0;
+            @Override
+            public boolean hasNext() {
+                return curr < data.length;
+            }
+
+            @Override
+            public T next() {
+                return data[curr];
+            }
+        };
+    }
+}
